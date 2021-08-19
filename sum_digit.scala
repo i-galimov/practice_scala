da5dba622818d95649235c7ab9c1e7abe4f9bbb9
Ilshat Galimov <86232244+ilshat1kzn@users.noreply.github.com>
2021-08-19 13:03:59 +0300

    Create sum_digit.scala

---
+++ sum_digit.scala
@@ -0,0 +1,9 @@
+// Напишите программу, которая считывает построчно два целых числа и считает их разницу.
+
+import scala.io.StdIn
+object Main extends App {
+  def a: Int = readInt()
+  def b: Int = readInt()
+  def c: Int = a - b
+  println(c)
+}
